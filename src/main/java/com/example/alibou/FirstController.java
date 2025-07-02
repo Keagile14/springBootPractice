7723ac2dddbc1db861e578a0dc3326bc0e0926c1
keagile14 <keagile1403@icloud.com>
2025-07-02 16:08:38 +0200

    updated code

--- src/main/java/com/example/alibou/FirstController.java
+++ src/main/java/com/example/alibou/FirstController.java
@@ -1,6 +1,9 @@
 package com.example.alibou;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,9 +17,16 @@ public class FirstController {
     }
 
     @GetMapping("/hello2")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public String sayHello2(){
         return "Hello2 from my first controller";
 
+    }
+    @PostMapping("/post")
+    public  String post(
+            String message
+    ){
+        return "Request accepted message is: " + message ;
     }
 
 
